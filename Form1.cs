82836fffc0cf0c26c50628ce73ed66b774c8157c
iniyo <inizle35@gmail.com>
2022-11-26 20:16:18 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace C_TemaProgramming
+{
+    public partial class Form1 : Form
+    {
+        DateTime today = DateTime.Today; // 현재날짜
+
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            // column을 추가합니다.
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("제목", typeof(string));
+            table.Columns.Add("구분", typeof(string));
+            table.Columns.Add("생성일", typeof(string));
+            table.Columns.Add("수정일", typeof(string));
+
+            // 각각의 행에 내용을 입력합니다.
+            table.Rows.Add("ID 1", "제목 1번", "사용중", "2019/03/11", "2019/03/18");
+            table.Rows.Add("ID 2", "제목 2번", "미사용", "2019/03/12", "2019/03/18");
+            table.Rows.Add("ID 3", "제목 3번", "미사용", "2019/03/13", "2019/03/18");
+            table.Rows.Add("ID 4", "제목 4번", "사용중", "2019/03/14", "2019/03/18");
+
+            // 값들이 입력된 테이블을 DataGridView에 입력합니다.
+            dataGridView1.DataSource = table;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void Date()
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
